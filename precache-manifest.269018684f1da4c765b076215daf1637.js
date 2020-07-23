1ae54d66e5beaa4c1b45c4c67c5c5ac6842f6a11
Meet Soni <meet.soni@emxcelsolutions.com>
2020-07-23 11:41:30 +0530

    Updates

---
+++ precache-manifest.269018684f1da4c765b076215daf1637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ca47a0075e51f0887427f2748d54b8",
+    "url": "/pick-your-color/index.html"
+  },
+  {
+    "revision": "47c6edf16cb69295f5df",
+    "url": "/pick-your-color/static/css/main.aab93563.chunk.css"
+  },
+  {
+    "revision": "af67e540d0c09c95d89a",
+    "url": "/pick-your-color/static/js/2.2fc8bd21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pick-your-color/static/js/2.2fc8bd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c6edf16cb69295f5df",
+    "url": "/pick-your-color/static/js/main.69c490c1.chunk.js"
+  },
+  {
+    "revision": "438a04bfaa6bb7ded99e",
+    "url": "/pick-your-color/static/js/runtime-main.4683a6c9.js"
+  }
+]);
